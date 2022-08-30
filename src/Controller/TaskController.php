d8033a8c630067249f80b41a7b17a165c44f0c71
QuentinHumbert <quentinhum76@gmail.com>
2022-08-30 10:57:50 +0200

    Finishing the CRUD

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -21,71 +22,185 @@ class TaskController extends AbstractController
     public function listing(ManagerRegistry $doctrine): Response
     {
         $tasks = $doctrine->getRepository(Task::class)->findall();
-        return $this->render('task/listing.html.twig', ['tasks'=>$tasks]);
+        return $this->render('task/listing.html.twig', ['tasks' => $tasks]);
     }
 
     #[Route('/task/add', name: 'app_add_task')]
-    public function add(): Response
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
+        // Instance de l'entité Task
         $task = new Task();
-        $task->setNameTask('Nommer votre tâche');
-        $task->setDescriptionTask('Decrire votre tâche');
+        // Utilisation des Setters sur le nom, la description et la Date d'expiration
+        $task->setNameTask('');
+        $task->setDescriptionTask('');
         $task->setDueDateTask(new \DateTime('tomorrow'));
-        $task->setPriorityTask('Choisir la priorité');
-        // $task->setCategory();
 
+        // Création du formulaire
         $form = $this->createFormBuilder($task)
-        ->add('nameTask', TextType::class, [
-            'attr' => [
-                'class'=>'form-control'
-                ]
-        ])
-        ->add('descriptionTask', TextareaType::class, [
-            'attr' => [
-                'class'=>'form-control'
-                ]
-        ])
-        ->add('dueDateTask', DateType::class, [
-            'widget'=>'single_text',
-            'attr' => [
-                'class'=>'form-control'
-                ]
-        ])
-        ->add('priorityTask', ChoiceType::class, [
-            'choices' => [
-                'Haute' => 'Haute',
-                'Normale' => 'Normale',
-                'Basse' => 'Basse',
-            ],
-            'attr' => [
-                'class'=>'form-control'
-                ]
-        ])
-        ->add('category', EntityType::class, [
-            'class' => Categories::class,
-            'choice_label' => 'libelleCategory'
-        ])
-        ->add('save', SubmitType::class, ['label' => 'Créer tâche'])
-        ->getForm();       
+            ->add('nameTask', TextType::class, [
+                'label' => 'Nom de la tâche',
 
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ecrivez votre tâche'
+                ]
+            ])
+            ->add('descriptionTask', TextareaType::class, [
+                'label' => 'Description de la tâche',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Décrivez votre tâche'
+                ]
+            ])
+            ->add('dueDateTask', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('priorityTask', ChoiceType::class, [
+                'label' => 'Priorité de la tâche',
+                'choices' => [
+                    'Haute' => 'Haute',
+                    'Normale' => 'Normale',
+                    'Basse' => 'Basse',
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie de la tâche',
+                'class' => Categories::class,
+                'choice_label' => 'libelleCategory',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer tâche',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Réccupére toutes les données du formalaire
+            $task = $form->getData();
+            $task->setCreatedDateTask(new \DateTime('now'));
+            $entityManager = $doctrine->getManager();
+            // Indique à la doctrine la sauvegarde (Pas de requête)
+            $entityManager->persist($task);
+            // Execute la requête 
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'La tâche a était créée.'
+            );
+
+            return $this->redirectToRoute('app_listing_task');
+        }
         return $this->render('task/add.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/task/update', name: 'app_update_task')]
-    public function update(): Response
+    #[Route('/task/update/{id}', name: 'app_update_task')]
+    public function update($id,Request $request, ManagerRegistry $doctrine): Response
     {
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        // Création du formulaire
+        $form = $this->createFormBuilder($task)
+            ->add('nameTask', TextType::class, [
+                'label' => 'Nom de la tâche',
+
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ecrivez votre tâche'
+                ]
+            ])
+            ->add('descriptionTask', TextareaType::class, [
+                'label' => 'Description de la tâche',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Décrivez votre tâche'
+                ]
+            ])
+            ->add('dueDateTask', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('priorityTask', ChoiceType::class, [
+                'label' => 'Priorité de la tâche',
+                'choices' => [
+                    'Haute' => 'Haute',
+                    'Normale' => 'Normale',
+                    'Basse' => 'Basse',
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie de la tâche',
+                'class' => Categories::class,
+                'choice_label' => 'libelleCategory',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer tâche',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Réccupére toutes les données du formalaire
+            $task = $form->getData();
+            $entityManager = $doctrine->getManager();
+            // Indique à la doctrine la sauvegarde (Pas de requête)
+            $entityManager->persist($task);
+            // Execute la requête 
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'La tâche a était modifié.'
+            );
+
+            return $this->redirectToRoute('app_listing_task');
+        }
+
         return $this->render('task/update.html.twig', [
-            'controller_name' => 'TaskController',
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/task/delete', name: 'app_delete_task')]
-    public function delete(): Response
+    #[Route('/task/delete/{id}', name: 'app_delete_task')]
+    public function delete($id, ManagerRegistry $doctrine): Response
     {
-        return $this->render('task/delete.html.twig', [
-            'controller_name' => 'TaskController',
-        ]);
+        $task = $doctrine->getRepository(Task::class)->find($id);
+        $entityManager = $doctrine->getManager();
+        // Indique la requête de suppression à la doctrine
+        $entityManager->remove($task);
+        // Execute la requête 
+        $entityManager->flush();
+
+        $this->addFlash(
+            'notice',
+            'La tâche a était supprimé.'
+        );
+
+        return $this->redirectToRoute('app_listing_task');
     }
 }
